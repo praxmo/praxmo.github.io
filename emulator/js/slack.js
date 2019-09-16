40f425d9cc90006f893467c83da2850fa83400af
lbirdeau <luke.birdeau@gmail.com>
2019-09-15 18:33:22 -0700

    animation events (ripples and highlights)

--- emulator/js/slack.js
+++ emulator/js/slack.js
@@ -228,15 +228,11 @@ function emulate(id, selector, slack_data) {
     },
     mounted: function () {
       this.$nextTick(function () {
-        this.scrollToEnd();
+        this.scroll_to_end();
       });
     },
     methods: {
-      addItem: function() {
-        this.items.push("Item #"+this.items.length);
-        this.scrollToEnd();
-      },
-      scrollToEnd: function() {
+      scroll_to_end: function() {
         //todo: pass the panel id needed by the query selector to target the correct subitem
         var container = this.$el.querySelector(".messages");
         if(container) {
@@ -245,45 +241,92 @@ function emulate(id, selector, slack_data) {
           });
         }
       },
-      setActiveChannel: function(panel_id, channel_id) {
+      set_active_channel: function(panel_id, channel_id) {
         this.panels[panel_id].activeChannelId = channel_id;
-        this.scrollToEnd();
+        //bind property that initiates the ripple effect
+        this.emphasize(this.panels[panel_id].channels[channel_id]);
+        var my = this;
+        Vue.nextTick(function() {
+          my.scroll_to_end();
+        });
+        //this.scroll_to_end();
       },
       //upload a file
       set_overlay: function(panel_id, b_show) {
         this.panels[panel_id].overlay = b_show;
+        this.emphasize(this.panels[panel_id]);
       },
       //start the narration
       play: function() {
         console.log('start playing');
         this.story_board.state = "playing";
       },
+      //add a new message
       send_message: function(panel_id) {
         var target = this.panels[panel_id].channels[this.panels[panel_id].activeChannelId].messages;
         var timestamp = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
-        var id = this.panels[panel_id].activeChannelId + "m" + new Date();
-        target[id] = {
+        var id = this.panels[panel_id].activeChannelId + "m" + new Date().valueOf();
+        var text = this.panels[panel_id].input;
+        var new_message = {
           id: id,
           author: this.panels[panel_id].activeUserId,
           timestamp: timestamp,
-          text: this.panels[panel_id].input
+          text: text
         };
-        //clear out text
+        if(text.endsWith("?")) {
+          //simulate a question with buttons if a question
+          new_message.actions = {
+            submit: {
+              id: "submit",
+              title: "Submit",
+              style: "primary"
+            },
+            cancel: {
+              id: "cancel",
+              title: "Cancel",
+              style: "danger"
+            }
+          }
+        }
+        //add using $set to bind the watcher
+        this.$set(target, id, new_message);
+        //clear out text from the input
         this.panels[panel_id].input = "";
-        this.scrollToEnd();
+        this.scroll_to_end();
+        var my = this;
+        Vue.nextTick(function() {
+          my.emphasize(new_message);
+        });
       },
+      //when an action button is clicked
       act: function(panel_id, channel, message, action) {
         var my = this;
+        this.emphasize(action);
         setTimeout(function() {
           var text = "✔️ You clicked the "  + action.id.toUpperCase() + " button.!";
           my.update_message(panel_id, channel.id, message.id, text);
-        },150);
+        },900);
       },
+      //update an existing message
       update_message: function(panel_id, channel_id, message_id, text) {
         var target = this.panels[panel_id].channels[this.panels[panel_id].activeChannelId].messages[message_id];
         delete target.actions;
         target.timestamp = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
         target.text = text;
+        this.emphasize(target);
+      },
+      //add new/updated message highlighting
+      emphasize: function(target) {
+        //use set (not part of original data set, so will not be watched otherwise)
+        this.$set(target, 'activating', true);
+        var my = this;
+        setTimeout(function() {
+          my.deemphasize(target);
+        },1500);
+      },
+      //remove new/updated message highlighting
+      deemphasize: function(target) {
+        this.$set(target, 'activating', false);
       }
     }
   });
