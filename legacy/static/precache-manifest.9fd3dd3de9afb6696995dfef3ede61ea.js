03855b2a3640562ed965a723c5085aa79635d807
lbirdeau <luke.birdeau@gmail.com>
2021-01-06 09:41:07 -0800

    deprecated main website 404 now returned

---
+++ legacy/static/precache-manifest.9fd3dd3de9afb6696995dfef3ede61ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e421901da7add3f8434dbf2fce1021cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d142c2b5b1e299d45920",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "52d6f48671d21392607a",
+    "url": "/static/js/2.e29da5c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e29da5c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d142c2b5b1e299d45920",
+    "url": "/static/js/main.4df7f5e2.chunk.js"
+  },
+  {
+    "revision": "2862e08b81687c178a81",
+    "url": "/static/js/runtime-main.b32688e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
